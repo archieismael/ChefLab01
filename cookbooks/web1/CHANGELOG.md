88ce381495003bf88dc8ec20c814d57b4cfd4eb5
Ubuntu <archieismaelyah@archieismaelyah.xll5uzktsrxefp1thibvj0bmnc.gx.internal.cloudapp.net>
2021-07-14 08:46:08 +0000

    Updates from Chef-Repo

---
+++ cookbooks/web1/CHANGELOG.md
@@ -0,0 +1,10 @@
+# web1 CHANGELOG
+
+This file is used to list changes made in each version of the web1 cookbook.
+
+## 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
---
+++ cookbooks/web1/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: web1
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+package "apache2" do
+  action :install
+end
+
+service "apache2" do
+  action [:enable, :restart]
+end
+
+file '/var/www/html/index.html' do
+  content 'Welcome to Apache Deployed Through Chef'
+end
